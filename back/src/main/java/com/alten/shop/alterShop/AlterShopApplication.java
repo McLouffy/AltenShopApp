baf4efdfa3f868bd16bb1c28032a541fb0e6e5a2
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-06-27 12:11:17 +0200

    start project

---
+++ back/src/main/java/com/alten/shop/alterShop/AlterShopApplication.java
@@ -0,0 +1,13 @@
+package com.alten.shop.alterShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlterShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlterShopApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/alten/shop/alterShop/AlterShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.shop.alterShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlterShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
