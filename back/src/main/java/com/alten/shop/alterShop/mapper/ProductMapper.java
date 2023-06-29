a0c52006047b83f4692b37c2b7b5ca0b5606cd97
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-06-29 21:34:24 +0200

    feat/add mapper dto entity

---
+++ back/src/main/java/com/alten/shop/alterShop/mapper/ProductMapper.java
@@ -0,0 +1,37 @@
+package com.alten.shop.alterShop.mapper;
+
+import com.alten.shop.alterShop.dto.ProductDto;
+import com.alten.shop.alterShop.model.Product;
+
+public class ProductMapper {
+
+	public static Product toEntity(ProductDto productDto) {
+		Product product = new Product();
+		product.setId(productDto.getId());
+		product.setCode(productDto.getCode());
+		product.setName(productDto.getName());
+		product.setDescription(productDto.getDescription());
+		product.setPrice(productDto.getPrice());
+		product.setQuantity(productDto.getQuantity());
+		product.setInventoryStatus(productDto.getInventoryStatus());
+		product.setCategory(productDto.getCategory());
+		product.setImage(productDto.getImage());
+		product.setRating(productDto.getRating());
+		return product;
+	}
+
+	public static ProductDto toDTO(Product product) {
+		ProductDto productDto = new ProductDto();
+		productDto.setId(product.getId());
+		productDto.setCode(product.getCode());
+		productDto.setName(product.getName());
+		productDto.setDescription(product.getDescription());
+		productDto.setPrice(product.getPrice());
+		productDto.setQuantity(product.getQuantity());
+		productDto.setInventoryStatus(product.getInventoryStatus());
+		productDto.setCategory(product.getCategory());
+		productDto.setImage(product.getImage());
+		productDto.setRating(product.getRating());
+		return productDto;
+	}
+}
