08714e03542966f6013ad56deade582d0bb91341
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-07-06 10:00:54 +0200

    add check condition

--- back/src/main/java/com/alten/shop/alterShop/repository/ProductRepository.java
+++ back/src/main/java/com/alten/shop/alterShop/repository/ProductRepository.java
@@ -9,5 +9,4 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	
-
 }
--- back/src/main/java/com/alten/shop/alterShop/service/impl/ProductServiceImpl.java
+++ back/src/main/java/com/alten/shop/alterShop/service/impl/ProductServiceImpl.java
@@ -11,109 +11,118 @@ import com.alten.shop.alterShop.dto.ProductDto;
 import com.alten.shop.alterShop.repository.ProductRepository;
 import com.alten.shop.alterShop.service.ProductService;
 
-
 /**
  * Implémentation du service ProductService pour la gestion des produits.
  */
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Override
-    public ProductDto createProduct(ProductDto productDto) {
-        // Convertir le DTO en entité
-        Product product = ProductMapper.toEntity(productDto);
-        // Sauvegarder le produit en base de données
-        Product createdProduct = productRepository.save(product);
-        // Convertir l'entité en DTO et le renvoyer
-        return ProductMapper.toDto(createdProduct);
-    }
+	@Override
+	public ProductDto createProduct(ProductDto productDto) {
+		// Convertir le DTO en entité
+		Product product = ProductMapper.toEntity(productDto);
+		// Sauvegarder le produit en base de données
+		Product createdProduct = productRepository.save(product);
+		// Convertir l'entité en DTO et le renvoyer
+		return ProductMapper.toDto(createdProduct);
+	}
 
-    @Override
-    public ProductDto getProductById(Long id) {
-        // Rechercher le produit par son ID en base de données
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
-        // Convertir l'entité en DTO et le renvoyer
-        return ProductMapper.toDto(product);
-    }
+	@Override
+	public ProductDto getProductById(Long id) {
+		
+		if (id==null) {
+   		 throw new IllegalArgumentException("ID must be provided");
+   	}
+		
+		// Rechercher le produit par son ID en base de données
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
+		// Convertir l'entité en DTO et le renvoyer
+		return ProductMapper.toDto(product);
+	}
 
-    @Override
-    public ProductDto updateProduct(Long id, ProductDto productDto) {
-        // Rechercher le produit existant par son ID en base de données
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
-        
-        // Mettre à jour les champs du produit avec les valeurs du DTO
-        product.setName(productDto.getName());
-        product.setPrice(productDto.getPrice());
-        // Mettre à jour les autres champs en fonction des besoins
-        
-        // Sauvegarder le produit mis à jour en base de données
-        Product updatedProduct = productRepository.save(product);
-        // Convertir l'entité mise à jour en DTO et le renvoyer
-        return ProductMapper.toDto(updatedProduct);
-    }
+	@Override
+	public ProductDto updateProduct(Long id, ProductDto productDto) {
+		
+		if (id==null) {
+   		 throw new IllegalArgumentException("ID must be provided");
+   	}
+		
+		// Rechercher le produit existant par son ID en base de données
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
 
-    @Override
-    public void deleteProduct(Long id) {
-        // Vérifier si le produit existe avant de le supprimer
-        if (!productRepository.existsById(id)) {
-            throw new ProductNotFoundException("Product not found with id: " + id);
-        }
-        // Supprimer le produit de la base de données
-        productRepository.deleteById(id);
-    }
+		// Mettre à jour les champs du produit avec les valeurs du DTO
+		product.setName(productDto.getName());
+		product.setPrice(productDto.getPrice());
+		// Mettre à jour les autres champs en fonction des besoins
 
-    @Override
-    public List<ProductDto> getAllProducts() {
-        // Récupérer tous les produits de la base de données
-        List<Product> products = productRepository.findAll();
-        // Convertir la liste d'entités en une liste de DTO et la renvoyer
-        return products.stream()
-                .map(ProductMapper::toDto)
-                .collect(Collectors.toList());
-    }
+		// Sauvegarder le produit mis à jour en base de données
+		Product updatedProduct = productRepository.save(product);
+		// Convertir l'entité mise à jour en DTO et le renvoyer
+		return ProductMapper.toDto(updatedProduct);
+	}
+
+	@Override
+	public void deleteProduct(Long id) {
+
+		if (id == null) {
+			throw new IllegalArgumentException("ID must be provided");
+		}
+
+		// Vérifier si le produit existe avant de le supprimer
+		if (!productRepository.existsById(id)) {
+			throw new ProductNotFoundException("Product not found with id: " + id);
+		}
+
+		// Supprimer le produit de la base de données
+		productRepository.deleteById(id);
+	}
+
+	@Override
+	public List<ProductDto> getAllProducts() {
+		// Récupérer tous les produits de la base de données
+		List<Product> products = productRepository.findAll();
+		// Convertir la liste d'entités en une liste de DTO et la renvoyer
+		return products.stream().map(ProductMapper::toDto).collect(Collectors.toList());
+	}
 
 	@Override
 	public ProductDto updateOneFieldInProduct(ProductDto productDto, Long id) {
-		
-		 // Rechercher le produit existant par son ID en base de données
-	    Product product = productRepository.findById(id)
-	            .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
 
-	    // Mettre à jour les champs spécifiques du produit avec les valeurs du DTO
-	    if (productDto.getName() != null) {
-	        product.setName(productDto.getName());
-	    }
-	    if (productDto.getPrice() != null) {
-	        product.setPrice(productDto.getPrice());
-	    }
-	    
-	    if (productDto.getDescription() != null) {
-	        product.setDescription(productDto.getDescription());
-	    }
-	    if (productDto.getQuantity() != null) {
-	        product.setQuantity(productDto.getQuantity());
-	    }
-	    //Ajout d'autres champs ) mettre à jours en fonction des besoins applicatifs
-	   
-	    
-	    // Sauvegarder le produit mis à jour en base de données
-	    Product updatedProduct = productRepository.save(product);
+		if (id == null) {
+			throw new IllegalArgumentException("ID must be provided");
+		}
 
-	    // Convertir l'entité mise à jour en DTO et le renvoyer
-	    return ProductMapper.toDto(updatedProduct);
-		
-		
+		// Rechercher le produit existant par son ID en base de données
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
+
+		// Mettre à jour les champs spécifiques du produit avec les valeurs du DTO
+		if (productDto.getName() != null) {
+			product.setName(productDto.getName());
+		}
+		if (productDto.getPrice() != null) {
+			product.setPrice(productDto.getPrice());
+		}
+
+		if (productDto.getDescription() != null) {
+			product.setDescription(productDto.getDescription());
+		}
+		if (productDto.getQuantity() != null) {
+			product.setQuantity(productDto.getQuantity());
+		}
+		// Ajout d'autres champs ) mettre à jour en fonction des besoins del'application
+
+		// Sauvegarder le produit mis à jour en base de données
+		Product updatedProduct = productRepository.save(product);
+
+		// Convertir l'entité mise à jour en DTO et le renvoyer
+		return ProductMapper.toDto(updatedProduct);
+
 	}
-    
-   
-    
-    
-
-
 
 }
